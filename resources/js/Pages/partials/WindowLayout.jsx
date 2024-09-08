73239276f9a0136475debb2fd70ce0ed227905e2
Aaron Fabian <aaronfabian78@gmail.com>
2024-09-08 22:02:10 +0900

    readjust UI

--- resources/js/Pages/partials/WindowLayout.jsx
+++ resources/js/Pages/partials/WindowLayout.jsx
@@ -1,6 +1,9 @@
 export default function WindowLayout({ children }) {
+    // some browser has different viewport height
+    const vh = window.innerHeight;
+
     return (
-        <div className="z-10 max-w-3xl mx-auto sm:scale-90 sm:-translate-y-1/2 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl sm:absolute sm:left-0 sm:right-0 sm:top-1/2">
+        <div className={`z-10 max-w-3xl mx-auto ${vh < 740 && 'sm:scale-90'} sm:-translate-y-1/2 lg:max-w-4xl xl:max-w-5xl 2xl:max-w-6xl sm:absolute sm:left-0 sm:right-0 sm:top-1/2`}>
             {children}
         </div>
     );
